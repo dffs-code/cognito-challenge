7349d663ed4f853f6c98b3ba35c94af56ceedb56
Daniel Formigoni <d.soares@aluno.ifsp.edu.br>
2024-08-22 16:26:48 -0300

    refactor update dockerfile and docker compose

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,14 @@
 import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '1234',
-  database: process.env.DB_NAME || 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [User],
   synchronize: false
 });
